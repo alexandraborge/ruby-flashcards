45f3c063cfe78d6f63c2fe619a22c6b9b31f8ecc
Alexandra Borge <alexandraroseingram@gmail.com>
2020-01-29 16:14:05 -0500

    Add user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/:id', to: 'users#show'
   root 'static_pages#home'
-  get 'static_pages/home'
   devise_for :users, path: 'users'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
