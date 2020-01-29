4a000f1cb3f929b4b9d2e0c52fcee88c7350e6e7
Alexandra Borge <alexandraroseingram@gmail.com>
2020-01-29 15:51:32 -0500

    Add root url

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get 'static_pages/home'
   devise_for :users, path: 'users'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
